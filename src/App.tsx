fd425cfefae3e23d029d5ad31715ce19ddf4a6d2
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-03-14 21:23:20 +0200

    routing setup, pages setup, tsconfig, eslint

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import './App.css';
+import { Routes, Route, Navigate, NavLink } from 'react-router-dom';
+import { HomePage } from './modules/HomePage';
+import { PhonesPage } from './modules/PhonesPage';
+import { TabletsPage } from './modules/TabletsPage';
+import { AccessoriesPage } from './modules/AccessoriesPage';
+import { FavoritesPage } from './modules/FavoritesPage';
+import { NotFoundPage } from './modules/NotFoundPage';
+import { ShoppingCartPage } from './modules/ShoppingCartPage';
+import { ProductDetailsPage } from './modules/ProductDetailsPage';
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="phones">phones</NavLink>
+        <NavLink to="tablets">tablets</NavLink>
+        <NavLink to="accessories">accessories</NavLink>
+        <NavLink to="cart">cart</NavLink>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="phones" >
+            <Route index element={<PhonesPage />} />
+
+            <Route path=":phoneId" element={<ProductDetailsPage />} />
+          </Route>
+
+          <Route path="tablets" element={<TabletsPage />} />
+
+          <Route path="accessories" element={<AccessoriesPage />} />
+
+          <Route path="cart" element={<ShoppingCartPage />} />
+
+          <Route path="favorites" element={<FavoritesPage />} />
+
+          <Route path="cart" element={<ShoppingCartPage />} />
+
+          <Route path="home" element={<Navigate to="/" replace />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+};
--- src/modules/moduleName/ModuleName.tsx
+++
@@ -1,5 +0,0 @@
-export const ModuleName = () => {
-    return (
-        <h1>Home Page</h1>
-    )
-}
