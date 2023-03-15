3cdd5dfc2499d88298c8e942d2cbbaa00cc3d8c9
Oleh <119847452+OlehSharha@users.noreply.github.com>
2023-03-15 11:27:12 +0200

    added first version of footer (#4)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import { App }  from './App';
+import React from 'react';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
