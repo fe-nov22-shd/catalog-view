3b9e479610ad5a7ab504c575acae14038869428d
OlehSharha <oleh.3sharga@gmail.com>
2023-03-15 16:02:22 +0200

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,34 +10,19 @@ export const  Footer = () => {
 
   return (
     <footer className='footer'>
-      <div className="footer-container" >
-        <div className="logo">
-          <img src={logo} alt="Logo" />
+      <div className="footer_container container" >
+        <div className="footer_logo logo">
+          <img onClick={handleScrollToTop} src={logo} alt="Logo" />
         </div>
-        <div>
-        <nav className="footer-nav">
-          <ul>
-            <li>
-              <Link className='link' to="https://github.com/fe-nov22-shd/catalog-view">GitHub</Link>
-            </li>
-            <li>
-              <Link className='link' to="/contacts">Contacts</Link>
-            </li>
-            <li>
-              <Link
-               className='link'
-                to="/rights"
-                >
-                  Rights
-                </Link>
-            </li>
-          </ul>
+        <nav className="footer_nav nav">
+              <Link className='nav_link' to="https://github.com/fe-nov22-shd/catalog-view">GitHub</Link>
+              <Link className='nav_link' to="/contacts">Contacts</Link>
+              <Link className='nav_link' to="/rights">Rights</Link>
         </nav>
-        </div>
-        <div className="scroll-to-top">
-          <p className='scroll-text'>Back to top</p>
-          <button onClick={handleScrollToTop}>
-            <img className='vector' src={vector} alt="Vector" />
+        <div className="footer_scroll scroll">
+          <p className='scroll_text'>Back to top</p>
+          <button className='scroll_button button' onClick={handleScrollToTop}>
+            <img src={vector} alt="Vector" />
           </button>
         </div>
       </div>
