e0ea546be9c182ef798d816174f0b93f7681f008
OlehSharha <oleh.3sharga@gmail.com>
2023-03-16 18:34:22 +0200

    new NotFoundPage

--- src/modules/NotFoundPage/NotFoundPage.tsx
+++ src/modules/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,12 @@
-export const NotFoundPage = () => {
-  return (
-    <h1>Page not found</h1>
-  );
-};
+import { Link } from 'react-router-dom';
+import errorImage from '../../img/Error.gif'
+import './NotFoundPage.scss'
+
+export const NotFoundPage = () => (
+  <div className="notFoundContainer">
+      <img src={errorImage} alt="404 error" className="error-image" />
+      <Link to="/">
+        <button className="notFoundContainer__back-button">Go back to HOME</button>
+      </Link>
+    </div>
+)
