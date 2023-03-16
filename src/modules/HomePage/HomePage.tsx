0dc673a61a74ef3d53b1e14860a076d55baf4222
Oleksandr Prokopenko <37651667+OleksandrProkop@users.noreply.github.com>
2023-03-16 13:09:40 +0200

    added grid component (#9)

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,5 +1,23 @@
+import Grid from '@mui/material/Grid';
+
+import './HomePage.scss';
+
 export const HomePage = () => {
+
   return (
-    <h1>Home Page</h1>
-  );
+    <Grid container rowSpacing={1} columnSpacing={3}>
+      <Grid item tablet={6} tabletXL={4} desktop={3} mobile={12}>
+        <p className="test">Item</p>
+      </Grid>
+      <Grid item tablet={6} tabletXL={4} desktop={3} mobile={12}>
+        <p className="test">Item</p>
+      </Grid>
+      <Grid item tablet={6} tabletXL={4} desktop={3} mobile={12}>
+        <p className="test">Item</p>
+      </Grid>
+      <Grid item tablet={6} tabletXL={4} desktop={3} mobile={12}>
+        <p className="test">Item</p>
+      </Grid>
+    </Grid>
+  )
 };
