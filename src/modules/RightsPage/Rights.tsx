8ecddf55b03d8feac426651f6cd6f34ada3c7b20
OlehSharha <oleh.3sharga@gmail.com>
2023-03-22 19:13:20 +0200

    added rights page

---
+++ src/modules/RightsPage/Rights.tsx
@@ -0,0 +1,111 @@
+import './Rights.scss';
+
+export const RightsPage = () => {
+  return (
+    <>
+    <h1 className="rights-block">
+      <h2 className="rights-block__title title" >User Conduct
+        <p className="rights-block__text" >
+        Users of the website must conduct themselves in a respectful and lawful manner. They may not post, upload, or share any content that is illegal, offensive, defamatory, harassing, or infringing on the rights of others.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Account Creation
+        <p className="rights-block__text" >
+          Users may be required to create an account to access certain features of the website. Users must provide accurate and truthful information when creating an account and are responsible for rights-blocktaining the security of their account.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Age Restrictions
+        <p className="rights-block__text" >
+        Users must be of legal age to use the website or have the permission of a legal guardian to do so.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Intellectual Property
+        <p className="rights-block__text" >
+        Users must respect the intellectual property rights of the website and other users. They may not use, reproduce, or distribute any content or materials from the website without permission.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Prohibited Activities
+        <p className="rights-block__text" >
+        Users may not use the website for any illegal or unauthorized purposes. This includes, but is not limited to, hacking, phishing, or distributing malware.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Privacy
+        <p className="rights-block__text" >
+        Users' personal information will be collected, used, and protected in accordance with the website's privacy policy.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Content Removal
+        <p className="rights-block__text" >
+        The website reserves the right to remove any content that violates these user policies or for any other reason.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Disclaimer
+        <p className="rights-block__text" >
+        The website does not endorse or guarantee the accuracy, reliability, or quality of any content on the website
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Liability
+        <p className="rights-block__text" >
+        The website is not liable for any damages or losses resulting from the use or inability to use the website, including but not limited to, loss of data, revenue, or profits.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Modifications
+        <p className="rights-block__text" >
+        The website reserves the right to modify these user policies at any time without notice. It is the user's responsibility to review these policies regularly.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Intellectual Property Rights
+        <p className="rights-block__text" >
+        The website has the right to own and protect its intellectual property rights, including trademarks, copyrights, and patents.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Privacy Policy
+        <p className="rights-block__text" >
+        The website has the right to establish a privacy policy that outlines how customer data is collected, used, and protected.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Terms of Use
+        <p className="rights-block__text" >
+        The website has the right to establish terms of use that outline how the site can be used and what actions are prohibited.
+
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Payment and Refund Policy
+        <p className="rights-block__text" >
+        The website has the right to establish a payment and refund policy that outlines how payments are processed and how refunds are issued.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Warranty and Returns Policy
+        <p className="rights-block__text" >
+        The website has the right to establish a warranty and returns policy that outlines how defective products are handled and how returns are processed.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Consumer Protection
+        <p className="rights-block__text" >
+        The website has the right to comply with all applicable consumer protection laws and regulations.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Security
+        <p className="rights-block__text" >
+        The website has the right to implement security measures to protect customer data and prevent unauthorized access.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Customer Service
+        <p className="rights-block__text" >
+        The website has the right to provide quality customer service, including responding to inquiries, resolving complaints, and providing support.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Advertising and Marketing
+        <p className="rights-block__text" >
+        The website has the right to advertise and market its products and services in compliance with applicable laws and regulations.
+        </p>
+      </h2>
+      <h2 className="rights-block__title title">Fair Competition
+        <p className="rights-block__text" >
+        The website has the right to engage in fair competition with other businesses in the same industry, while complying with applicable laws and regulations.
+        </p>
+      </h2>
+    </h1>
+    </>
+  );
+};
