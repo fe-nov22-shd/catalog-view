39dd3053dc0c016bd670cc675e73427199fd48a1
OlehSharha <oleh.3sharga@gmail.com>
2023-03-24 12:01:29 +0200

    something for breadcrumbs and transition for contact us

--- src/modules/Breadcrumbs/Breadcrumbs.tsx
+++ src/modules/Breadcrumbs/Breadcrumbs.tsx
@@ -25,12 +25,12 @@ export const Breadcrumbs = () => {
           const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
           const isLast = index === pathnames.length - 1;
           return isLast ? (
-            <li key={name} className="breadcrumb-item active" aria-current="page">
+            <li key={name} className="breadcrumb-item active actimel" aria-current="page">
               <img src={vector} alt="vector-right" className="vector"  />
-                {name.slice(0, 1).toUpperCase() + name.slice(1).replace(/-/g, ' ')}
+              {name.slice(0, 1).toUpperCase() + name.slice(1).replace(/-/g, ' ')}
             </li>
           ) : (
-            <li key={name} className="breadcrumb-item">
+            <li key={name} className="breadcrumb-item link-width">
               <Link to={routeTo} className="breadcrumb-link">
                 <img
                   src={vector}
